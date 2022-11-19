3042d5b8cfc6243312eccbfa71e9f288f1956e0a
Richard Ngo <richardngo04@gmail.com>
2022-11-18 21:34:24 -0800

    updating script to filter broken images

--- jsonreader.py
+++ jsonreader.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import requests
 
 #reading in the json file
 with open("atlas_dataset.json", "r") as jsonfile:
@@ -11,20 +12,37 @@ with open("atlas_dataset.json", "r") as jsonfile:
 # print(datapoint["sentences"][0]['tokens'])
 # print(datapoint["image_url"])
 
-output_list = [["Name", "URL", "tokens"]]
+def checkpoint(checkpoint_location):
+    with open("CleanURLOnly{}.csv".format(count), 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(output_list)
 
+
+
+output_list = [["Name", "URL", "tokens"]]
+count = 0
 
 for datapoint in dataset["images"]:
     buffer = [str(datapoint["title"]).replace("\n", " "), "Error"]
     tokens = datapoint["sentences"][0]['tokens']
     buffer.extend(tokens)
 
+
+
     if "voonik" not in datapoint["image_url"]:
-        buffer[1] = datapoint["image_url"]
+        url = datapoint["image_url"]
 
-        output_list.append(buffer)
+        try:
+            response = requests.get(url)
 
+            if (response.status_code == 200):
+                buffer[1] = url
+                output_list.append(buffer)
+                count +=1
 
-with open("CleanURLOnly.csv", 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(output_list)
+                if count % 5000 == 0:
+                    checkpoint(count)
+        except:
+            pass
+
+checkpoint("final")
